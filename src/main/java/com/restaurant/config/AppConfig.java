c6d44d6b7ef08ae7d4142c0457ab4f493e873316
Đăng Việt <trandangviet00@gmail.com>
2026-04-09 10:27:09 +0700

    upload

--- src/main/java/com/restaurant/config/AppConfig.java
+++ src/main/java/com/restaurant/config/AppConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import java.util.Scanner;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.restaurant")
